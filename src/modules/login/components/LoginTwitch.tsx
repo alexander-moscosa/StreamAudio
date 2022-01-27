36c3a6e643404ce39204d47967fa328281866e04
Javier Zetina <zacag1500@gmail.com>
2022-01-27 08:45:46 -0600

    Comunicacion entre componentes con sessionStorage

--- src/modules/login/components/LoginTwitch.tsx
+++ src/modules/login/components/LoginTwitch.tsx
@@ -1,14 +1,24 @@
-import { LoginTwitchProps } from "../interfaces/interfaces";
+import { LoginTwitchProps } from '../interfaces/interfaces';
 
-export const LoginTwitch = ( { client_id, redirect_uri, scope }: LoginTwitchProps ) => {
+export const LoginTwitch = ({
+  client_id,
+  redirect_uri,
+  scope,
+}: LoginTwitchProps) => {
+  const handleButtonClick = () => {
+    window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope.join(
+      ' ',
+    )}`;
+
+    sessionStorage.setItem('meta', JSON.stringify({
+      loggingIn: true,
+    }));
+  };
+
   return (
     <div className="loginTwitch">
       <div className="twitchButton">
-        <a
-          href={`https://id.twitch.tv/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token&scope=${ scope.join(' ') }`}
-        >
-          Iniciar Sesión con Twitch
-        </a>
+        <button onClick={handleButtonClick}>Iniciar Sesión con Twitch</button>
       </div>
     </div>
   );
